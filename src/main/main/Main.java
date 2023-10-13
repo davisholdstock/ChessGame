f501d25e518fae8e34ce575aca25e6775d240039
Davis Holdstock <daviswholdstock@gmail.com>
2023-10-13 11:30:38 -0600

    started to impliment a GUI

--- src/main/main/Main.java
+++ src/main/main/Main.java
@@ -1,6 +1,5 @@
 package main;
 
-import chess.ChessGame;
 import chess.InvalidMoveException;
 
 import java.io.IOException;
@@ -8,25 +7,27 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String[] args) throws IOException, InvalidMoveException {
-        ChessGame game = new Game();
+        final ChessGUI app = new ChessGUI();
 
-        game.getBoard().resetBoard();
-        System.out.println(game.toString());
-
-        game.makeMove(new Move(new Position(1, 5), new Position(2, 5), null));
-        System.out.println(game.toString());
-
-        game.makeMove(new Move(new Position(6, 4), new Position(4, 4), null));
-        System.out.println(game.toString());
-
-        game.makeMove(new Move(new Position(1, 6), new Position(3, 6), null));
-        System.out.println(game.toString());
-
-        game.makeMove(new Move(new Position(7, 3), new Position(3, 7), null));
-        System.out.println(game.toString());
-
-        if (game.isInCheckmate(ChessGame.TeamColor.WHITE))
-            System.out.println("TRUE");
+//        ChessGame game = new Game();
+//
+//        game.getBoard().resetBoard();
+//        System.out.println(game.toString());
+//
+//        game.makeMove(new Move(new Position(1, 5), new Position(2, 5), null));
+//        System.out.println(game.toString());
+//
+//        game.makeMove(new Move(new Position(6, 4), new Position(4, 4), null));
+//        System.out.println(game.toString());
+//
+//        game.makeMove(new Move(new Position(1, 6), new Position(3, 6), null));
+//        System.out.println(game.toString());
+//
+//        game.makeMove(new Move(new Position(7, 3), new Position(3, 7), null));
+//        System.out.println(game.toString());
+//
+//        if (game.isInCheckmate(ChessGame.TeamColor.WHITE))
+//            System.out.println("TRUE");
 
     }
 }
