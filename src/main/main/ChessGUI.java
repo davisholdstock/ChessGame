d6ffaede1823ca11e2f4c5e333c279b291227d08
Davis Holdstock <daviswholdstock@gmail.com>
2023-10-14 10:58:30 -0600

    created a GUI

--- src/main/main/ChessGUI.java
+++ src/main/main/ChessGUI.java
@@ -1,5 +1,8 @@
 package main;
 
+import chess.ChessGame;
+import chess.ChessPiece;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.MouseEvent;
@@ -8,8 +11,9 @@ import java.awt.event.MouseListener;
 public class ChessGUI extends JFrame implements MouseListener {
     private int windowHeight = 500;
     private int windowWidth = 475;
-    private JPanel[][] chessGameBoard = new JPanel[8][8];
-    private JPanel pnlMain = new JPanel(new GridLayout(8, 8));
+    ChessGame game = new Game();
+    private JPanel[][] chessGameBoard = new JPanel[game.getBoard().getRows()][game.getBoard().getColumns()];
+    private JPanel pnlMain = new JPanel(new GridLayout(game.getBoard().getRows(), game.getBoard().getColumns()));
     private String[][] strChessBoard = new String[][]{{"RB", "NB", "BB", "QB", "KB", "BB", "NB", "RB"}, {"PB", "PB", "PB", "PB", "PB", "PB", "PB", "PB"}, {"  ", "  ", "  ", "  ", "  ", "  ", "  ", "  "}, {"  ", "  ", "  ", "  ", "  ", "  ", "  ", "  "}, {"  ", "  ", "  ", "  ", "  ", "  ", "  ", "  "}, {"  ", "  ", "  ", "  ", "  ", "  ", "  ", "  "}, {"PW", "PW", "PW", "PW", "PW", "PW", "PW", "PW"}, {"RW", "NW", "BW", "QW", "KW", "BW", "NW", "RW"}};
     private ImageIcon rookBlack = new ImageIcon(new ImageIcon("C:/Users/davis/IdeaProjects/ChessGamePhase1/src/main/main/images/b_rook.png").getImage().getScaledInstance(windowWidth / 9, windowHeight / 9, Image.SCALE_SMOOTH));
     private ImageIcon rookWhite = new ImageIcon(new ImageIcon("C:/Users/davis/IdeaProjects/ChessGamePhase1/src/main/main/images/w_rook.png").getImage().getScaledInstance(windowWidth / 9, windowHeight / 9, Image.SCALE_SMOOTH));
@@ -30,31 +34,19 @@ public class ChessGUI extends JFrame implements MouseListener {
     // the whole constructor is for setting up the UI of the form
 
     public ChessGUI() {
-
+        game.getBoard().resetBoard();
         c = getContentPane();
-
         setBounds(100, 100, windowWidth, windowHeight);
-
         setBackground(new Color(204, 204, 204));
-
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-
-        setTitle("Tutorial: How to Draw a Chess Board in Java");
-
+        setTitle("Chess Game");
         setResizable(false);
-
         c.setLayout(null);
-
         pnlMain.setBounds(0, 0, windowWidth - 15, windowHeight - 40);
-
         pnlMain.setBackground(new Color(255, 255, 255));
-
         c.add(pnlMain);
-
         this.drawChessBoard();
-
         this.arrangeChessPieces();
-
         show();
 
     }
@@ -66,86 +58,50 @@ public class ChessGUI extends JFrame implements MouseListener {
      */
 
     public void mouseClicked(MouseEvent e) {
-
         if (bMyTurn) {
-
             Object source = e.getSource();
-
             JPanel pnlTemp = (JPanel) source;
-
             int intX = (pnlTemp.getX() / 57);
-
             int intY = (pnlTemp.getY() / 57);
-
             this.boolMoveSelection = !this.boolMoveSelection;
-
             if (this.boolMoveSelection) {
-
                 this.pntMoveFrom = new Point(intX, intY);
-
-                if (this.strChessBoard[this.pntMoveFrom.y][this.pntMoveFrom.x].toString().trim().equals(""))
-
+                if (this.strChessBoard[this.pntMoveFrom.y][this.pntMoveFrom.x].toString().trim().equals("") && game.getBoard().getPiece(new Position(this.pntMoveFrom.y, this.pntMoveFrom.x)) == null)
                     this.boolMoveSelection = !this.boolMoveSelection;
-
                 if ((!this.strChessBoard[this.pntMoveFrom.y][this.pntMoveFrom.x].toString().trim().equals("")) &&
-
-                        this.bWhite && this.strChessBoard[this.pntMoveFrom.y][this.pntMoveFrom.x].toString().charAt(1) == 'B')
-
+                        game.getBoard().getPiece(new Position(this.pntMoveFrom.y, this.pntMoveFrom.x)) != null &&
+                        !this.bWhite /*&& this.strChessBoard[this.pntMoveFrom.y][this.pntMoveFrom.x].toString().charAt(1) == 'B'*/)
                     this.boolMoveSelection = !this.boolMoveSelection;
-
                 if ((!this.strChessBoard[this.pntMoveFrom.y][this.pntMoveFrom.x].toString().trim().equals("")) &&
-
-                        !this.bWhite && this.strChessBoard[this.pntMoveFrom.y][this.pntMoveFrom.x].toString().charAt(1) == 'W')
-
+                        game.getBoard().getPiece(new Position(this.pntMoveFrom.y, this.pntMoveFrom.x)) != null &&
+                        !this.bWhite /*&& this.strChessBoard[this.pntMoveFrom.y][this.pntMoveFrom.x].toString().charAt(1) == 'W'*/)
                     this.boolMoveSelection = !this.boolMoveSelection;
-
                 if (this.boolMoveSelection)
-
                     this.makeChessPieceDifferent(true);
-
             } else {
-
                 this.pntMoveTo = new Point(intX, intY);
-
                 if (!this.pntMoveFrom.equals(this.pntMoveTo)) {
-
                     if (this.strChessBoard[this.pntMoveFrom.y][this.pntMoveFrom.x].toString().trim() != "")
-
                         if (this.isMoveValid()) {
-
                             this.strChessBoard[this.pntMoveTo.y][this.pntMoveTo.x] = this.strChessBoard[this.pntMoveFrom.y][this.pntMoveFrom.x].toString();
-
                             this.strChessBoard[this.pntMoveFrom.y][this.pntMoveFrom.x] = "  ";
-
                             this.moveChessPiece();
-
                         } else {
-
                             JOptionPane.showMessageDialog(this, "Invalid Move Request.", "Warning", JOptionPane.ERROR_MESSAGE);
-
                             this.makeChessPieceDifferent(false);
-
                         }
-
                 } else
-
                     this.makeChessPieceDifferent(false);
-
             }
-
         }
-
     }
 
     /**
      * This method checks if attempted move is valid or not
      */
     private boolean isMoveValid() {
-
         boolean isMoveValid = true;
-
         return isMoveValid;
-
     }
 
     /**
@@ -154,17 +110,11 @@ public class ChessGUI extends JFrame implements MouseListener {
      * @param bSelected
      */
     private void makeChessPieceDifferent(boolean bSelected) {
-
         for (int z = 0; z < this.chessGameBoard[this.pntMoveFrom.y][this.pntMoveFrom.x].getComponentCount(); z++)
-
-            if (this.chessGameBoard[this.pntMoveFrom.y][this.pntMoveFrom.x].getComponent(z).getClass().toString().indexOf("JLabel") > -1) {
-
+            if (this.chessGameBoard[this.pntMoveFrom.y][this.pntMoveFrom.x].getComponent(z).getClass().toString().contains("JLabel")) {
                 JLabel lblTemp = (JLabel) this.chessGameBoard[this.pntMoveFrom.y][this.pntMoveFrom.x].getComponent(z);
-
                 lblTemp.setEnabled(!bSelected);
-
             }
-
     }
 
     /**
@@ -173,98 +123,69 @@ public class ChessGUI extends JFrame implements MouseListener {
      * one cell to the other
      */
     private void moveChessPiece() {
-
         for (int z = 0; z < this.chessGameBoard[this.pntMoveTo.y][this.pntMoveTo.x].getComponentCount(); z++)
-
-            if (this.chessGameBoard[this.pntMoveTo.y][this.pntMoveTo.x].getComponent(z).getClass().toString().indexOf("JLabel") > -1) {
-
+            if (this.chessGameBoard[this.pntMoveTo.y][this.pntMoveTo.x].getComponent(z).getClass().toString().contains("JLabel")) {
                 this.chessGameBoard[this.pntMoveTo.y][this.pntMoveTo.x].remove(z);
-
                 this.chessGameBoard[this.pntMoveTo.y][this.pntMoveTo.x].repaint();
-
             }
-
         for (int z = 0; z < this.chessGameBoard[this.pntMoveFrom.y][this.pntMoveFrom.x].getComponentCount(); z++)
-
-            if (this.chessGameBoard[this.pntMoveFrom.y][this.pntMoveFrom.x].getComponent(z).getClass().toString().indexOf("JLabel") > -1) {
-
+            if (this.chessGameBoard[this.pntMoveFrom.y][this.pntMoveFrom.x].getComponent(z).getClass().toString().contains("JLabel")) {
                 this.chessGameBoard[this.pntMoveFrom.y][this.pntMoveFrom.x].remove(z);
-
                 this.chessGameBoard[this.pntMoveFrom.y][this.pntMoveFrom.x].repaint();
-
             }
-
-        this.chessGameBoard[this.pntMoveTo.y][this.pntMoveTo.x].add(this.getPieceObject(this.strChessBoard[this.pntMoveTo.y][this.pntMoveTo.x]), BorderLayout.CENTER);
-
+        this.chessGameBoard[this.pntMoveTo.y][this.pntMoveTo.x].add(this.getPieceObject(game.getBoard().getPiece(new Position(this.pntMoveTo.y, this.pntMoveTo.x))), BorderLayout.CENTER);
         this.chessGameBoard[this.pntMoveTo.y][this.pntMoveTo.x].validate();
-
     }
 
     /**
      * Given the code of a piece as a string, this method instantiates
      * a label object with the right image inside it
      *
-     * @param strPieceName
+     * @param chessPiece
      * @return
      */
-    private JLabel getPieceObject(String strPieceName) {
-
+    private JLabel getPieceObject(ChessPiece chessPiece) {
         JLabel lblTemp;
-
-        if (strPieceName.equals("RB"))
-
-            lblTemp = new JLabel(this.rookBlack);
-
-        else if (strPieceName.equals("BB"))
-
-            lblTemp = new JLabel(this.bishopBlack);
-
-        else if (strPieceName.equals("NB"))
-
-            lblTemp = new JLabel(this.knightBlack);
-
-        else if (strPieceName.equals("QB"))
-
-            lblTemp = new JLabel(this.queenBlack);
-
-        else if (strPieceName.equals("KB"))
-
-            lblTemp = new JLabel(this.kingBlack);
-
-        else if (strPieceName.equals("PB"))
-
-            lblTemp = new JLabel(this.pawnBlack);
-
-        else if (strPieceName.equals("RW"))
-
-            lblTemp = new JLabel(this.rookWhite);
-
-        else if (strPieceName.equals("BW"))
-
-            lblTemp = new JLabel(this.bishopWhite);
-
-        else if (strPieceName.equals("NW"))
-
-            lblTemp = new JLabel(this.knightWhite);
-
-        else if (strPieceName.equals("QW"))
-
-            lblTemp = new JLabel(this.queenWhite);
-
-        else if (strPieceName.equals("KW"))
-
-            lblTemp = new JLabel(this.kingWhite);
-
-        else if (strPieceName.equals("PW"))
-
-            lblTemp = new JLabel(this.pawnWhite);
-
+        if (chessPiece == null)
+            return new JLabel();
+        if (chessPiece.getPieceType() == ChessPiece.PieceType.ROOK
+                && chessPiece.getTeamColor() == ChessGame.TeamColor.BLACK)
+            return new JLabel(this.rookBlack);
+        else if (chessPiece.getPieceType() == ChessPiece.PieceType.BISHOP
+                && chessPiece.getTeamColor() == ChessGame.TeamColor.BLACK)
+            return new JLabel(this.bishopBlack);
+        else if (chessPiece.getPieceType() == ChessPiece.PieceType.KNIGHT
+                && chessPiece.getTeamColor() == ChessGame.TeamColor.BLACK)
+            return new JLabel(this.knightBlack);
+        else if (chessPiece.getPieceType() == ChessPiece.PieceType.QUEEN
+                && chessPiece.getTeamColor() == ChessGame.TeamColor.BLACK)
+            return new JLabel(this.queenBlack);
+        else if (chessPiece.getPieceType() == ChessPiece.PieceType.KING
+                && chessPiece.getTeamColor() == ChessGame.TeamColor.BLACK)
+            return new JLabel(this.kingBlack);
+        else if (chessPiece.getPieceType() == ChessPiece.PieceType.PAWN
+                && chessPiece.getTeamColor() == ChessGame.TeamColor.BLACK)
+            return new JLabel(this.pawnBlack);
+        else if (chessPiece.getPieceType() == ChessPiece.PieceType.ROOK
+                && chessPiece.getTeamColor() == ChessGame.TeamColor.WHITE)
+            return new JLabel(this.rookWhite);
+        else if (chessPiece.getPieceType() == ChessPiece.PieceType.BISHOP
+                && chessPiece.getTeamColor() == ChessGame.TeamColor.WHITE)
+            return new JLabel(this.bishopWhite);
+        else if (chessPiece.getPieceType() == ChessPiece.PieceType.KNIGHT
+                && chessPiece.getTeamColor() == ChessGame.TeamColor.WHITE)
+            return new JLabel(this.knightWhite);
+        else if (chessPiece.getPieceType() == ChessPiece.PieceType.QUEEN
+                && chessPiece.getTeamColor() == ChessGame.TeamColor.WHITE)
+            return new JLabel(this.queenWhite);
+        else if (chessPiece.getPieceType() == ChessPiece.PieceType.KING
+                && chessPiece.getTeamColor() == ChessGame.TeamColor.WHITE)
+            return new JLabel(this.kingWhite);
+        else if (chessPiece.getPieceType() == ChessPiece.PieceType.PAWN
+                && chessPiece.getTeamColor() == ChessGame.TeamColor.WHITE)
+            return new JLabel(this.pawnWhite);
         else
-
-            lblTemp = new JLabel();
-
-        return lblTemp;
-
+            return new JLabel();
     }
 
     /**
@@ -272,54 +193,32 @@ public class ChessGUI extends JFrame implements MouseListener {
      * and places chess pieces at their right positions
      */
     private void arrangeChessPieces() {
-
-        for (int y = 0; y < 8; y++)
-
-            for (int x = 0; x < 8; x++) {
-
-                this.chessGameBoard[y][x].add(this.getPieceObject(strChessBoard[y][x]), BorderLayout.CENTER);
-
+        for (int y = 0; y < game.getBoard().getColumns(); ++y)
+            for (int x = 0; x < game.getBoard().getRows(); ++x) {
+                this.chessGameBoard[y][x].add(this.getPieceObject(game.getBoard().getPiece(new Position((game.getBoard().getColumns() - 1) - y, x))), BorderLayout.CENTER);
                 this.chessGameBoard[y][x].validate();
-
             }
-
     }
 
     /**
      * This method draws chess board, i.e. black and white cells on the board
      */
     private void drawChessBoard() {
-
-        for (int y = 0; y < 8; y++)
-
-            for (int x = 0; x < 8; x++) {
-
+        for (int y = 0; y < game.getBoard().getColumns(); ++y)
+            for (int x = 0; x < game.getBoard().getRows(); ++x) {
                 chessGameBoard[y][x] = new JPanel(new BorderLayout());
-
                 chessGameBoard[y][x].addMouseListener(this);
-
                 pnlMain.add(chessGameBoard[y][x]);
-
                 if (y % 2 == 0)
-
                     if (x % 2 != 0)
-
                         chessGameBoard[y][x].setBackground(Color.DARK_GRAY);
-
                     else
-
                         chessGameBoard[y][x].setBackground(Color.WHITE);
-
                 else if (x % 2 == 0)
-
                     chessGameBoard[y][x].setBackground(Color.DARK_GRAY);
-
                 else
-
                     chessGameBoard[y][x].setBackground(Color.WHITE);
-
             }
-
     }
 
     public void mouseEntered(MouseEvent e) {
