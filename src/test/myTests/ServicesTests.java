c3340469c08b030885099dfbbdbf86a2300bf40c
Davis Holdstock <daviswholdstock@gmail.com>
2023-10-27 08:20:50 -0600

    Logout service works

--- src/test/myTests/ServicesTests.java
+++ src/test/myTests/ServicesTests.java
@@ -10,6 +10,7 @@ import server.server;
 import service.AuthService;
 import service.LoginRequest;
 import service.LoginResponse;
+import service.LogoutResponse;
 
 import java.util.HashMap;
 
@@ -36,59 +37,20 @@ public class ServicesTests {
         Assertions.assertEquals((new LoginResponse("user", "12")).toString(), (authService.TestLogin(new LoginRequest("user", "password", "user.password@gmail.com"), "12")).toString());
     }
 
-//    @Test
-//    @DisplayName("Add duplicate")
-//    public void addUserFail() throws DataAccessException {
-//        User user = new User("user", "password", "user.password@gmail.com");
-//        db.writeUser(user);
-//
-//        Assertions.assertThrows(DataAccessException.class, () -> db.writeUser(user));
-//    }
-//
-//    @Test
-//    @DisplayName("Add and Get Game")
-//    public void getAddGameSuccess() throws DataAccessException {
-//        ChessGame chessGame = new main.Game();
-//        Game game = new model.Game("game1", chessGame, "whiteUser", "blackUser", 123);
-//        db.writeGame(game);
-//
-//        Game foundGame = db.readGame(game.gameID());
-//
-//        Assertions.assertNotEquals(new HashMap<>(), db.getGames());
-//        Assertions.assertEquals(game.toString(), foundGame.toString(),
-//                "Wrong Game Found");
-//    }
-//
-//    @Test
-//    @DisplayName("List All Games")
-//    public void listAllGames() throws DataAccessException {
-//        ChessGame chessGame = new main.Game();
-//        Game game = new model.Game("game0", chessGame, "whiteUser", "blackUser", 123);
-//        Game game1 = new model.Game("game1", chessGame, "whiteUser", "blackUser", 1234);
-//        Game game2 = new model.Game("game2", chessGame, "whiteUser", "blackUser", 12345);
-//        Game game3 = new model.Game("game3", chessGame, "whiteUser", "blackUser", 123456);
-//        db.writeGame(game);
-//        db.writeGame(game1);
-//        db.writeGame(game2);
-//        db.writeGame(game3);
-//
-//        ArrayList<Game> foundGames = db.readAllGame();
-//
-//        Assertions.assertNotEquals(new HashMap<>(), db.getGames());
-//        Assertions.assertEquals(4, foundGames.size(),
-//                "Wrong Games Found");
-//    }
-//
-//    @Test
-//    @DisplayName("Add and Get Auth")
-//    public void getAddAuthSuccess() throws DataAccessException {
-//        AuthToken authtoken = new AuthToken("yes", "user");
-//        db.writeAuth(authtoken);
-//
-//        AuthToken foundAuth = db.readAuth(authtoken);
-//
-//        Assertions.assertNotEquals(new HashMap<>(), db.getAuths());
-//        Assertions.assertEquals(authtoken, foundAuth,
-//                "Wrong Auth Found");
-//    }
+    @Test
+    @DisplayName("Logout User")
+    public void logoutSuccess() throws DataAccessException {
+        User user = new User("user", "password", "user.password@gmail.com");
+        server.db.writeUser(user);
+
+        Assertions.assertNotEquals(new HashMap<>(), server.db.getUsers());
+        User founduser = server.db.readUser(user.username());
+        Assertions.assertEquals(user.toString(), founduser.toString(),
+                "Wrong User Found");
+
+        Assertions.assertEquals((new LoginResponse("user", "12")).toString(), (authService.TestLogin(new LoginRequest("user", "password", "user.password@gmail.com"), "12")).toString());
+
+        Assertions.assertNotEquals((new LogoutResponse("Error: unauthorized")).toString(), (authService.Logout("user")).toString());
+        Assertions.assertEquals(new HashMap<>(), server.db.getUsers());
+    }
 }
