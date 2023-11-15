84b22d5317a7bfecf33721c290635d349a41d9c3
Davis Holdstock <daviswholdstock@gmail.com>
2023-11-15 15:28:17 -0700

    started(client ui)

--- client/src/main/ui/clientMain.java
+++ client/src/main/ui/clientMain.java
@@ -1,12 +1,46 @@
 package ui;
 
-import chess.ChessBoard;
-import main.Board;
+import java.util.Scanner;
 
 public class clientMain {
     public static void main(String[] args) {
-        ChessBoard board = new Board();
-        board.resetBoard();
-        System.out.println(board.toFENNotation());
+        var serverUrl = "http://localhost:8080";
+        if (args.length == 1) {
+            serverUrl = args[0];
+        }
+
+//        new Repl(serverUrl).run();
+        String option = "logged_out";
+
+        while (!option.toLowerCase().equals("quit")) {
+            System.out.printf("\n[" + option.toUpperCase() + "]%n>>> ");
+            Scanner scanner = new Scanner(System.in);
+            String line = scanner.nextLine();
+            var numbers = line.split(" ");
+
+            switch (numbers[0]) {
+                case "login" -> {
+                    option = "logged_in";
+                }
+                case "help" -> {
+                    switch (option.toLowerCase()) {
+                        case "logged_out" -> {
+                            System.out.println("register <USERNAME> <PASSWORD> <EMAIL> - to create an account");
+                            System.out.println("login <USERNAME> <PASSWORD>");
+                            System.out.println("quit - playing chess");
+                            System.out.println("help - with possible commands");
+                        }
+                        default -> {
+                        }
+                    }
+                }
+                case "quit" -> {
+                    option = "quit";
+                }
+                default -> {
+                    option = "logged_out";
+                }
+            }
+        }
     }
 }
